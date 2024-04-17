8316942184837b7d49ead64a26b6ec717dc6d990
Jefferson Barbosa <119863688+Jefferson-BR@users.noreply.github.com>
2024-04-17 08:33:44 -0300

    Update nota-media-do-aluno.py

--- Módulo 1/Desafios/D-007 (Concluido)/nota-media-do-aluno.py
+++ Módulo 1/Desafios/D-007 (Concluido)/nota-media-do-aluno.py
@@ -1,9 +1,6 @@
 nota_1 = float(input('Digite a primeira nota: '))
 nota_2 = float(input('Digite a segunda nota: '))
-nota_3 = float(input('Digite a Terceira nota: '))
-nota_4 = float(input('Digite a quarta nota: '))
 
-soma = nota_1 + nota_2 + nota_3 + nota_4
-media = soma / 4
+media = (nota_1 + nota_2) / 2
 
 print(f'Sua nota final é: {media:.1f}')
