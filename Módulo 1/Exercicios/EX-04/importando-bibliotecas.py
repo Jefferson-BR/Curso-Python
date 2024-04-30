bacac3b5e829e5425b9ce88f63e692d0defc5ddf
Jefferson Barbosa <119863688+Jefferson-BR@users.noreply.github.com>
2024-04-30 10:42:04 -0300

    Add files via upload

---
+++ Módulo 1/Exercicios/EX-04/importando-bibliotecas.py
@@ -0,0 +1,8 @@
+from math import sqrt, ceil, floor
+import random
+
+# numero = int(input('Digite um número: '))
+# raiz_quadrada = sqrt(numero)
+# print('A raiz_quadrada de {} arredondando para cima (math.ceil) fica: {} arredondando para baixo (math.floor) fica: {}'.format(numero, ceil(raiz_quadrada), floor(raiz_quadrada)))
+
+print(random.random(),'\n',random.randint(1, 25))
